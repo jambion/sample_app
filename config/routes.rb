063f304ad369c489278c96a3d8ef0fbada2dcb73
jambion <Ambion_J@yahoo.com>
2017-06-02 21:03:33 +0000

    Created About page

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,12 @@ Rails.application.routes.draw do
   get 'static_pages/home'
 
   get 'static_pages/help'
+  
+  # Tells Rails to route a GET request for the URL /static_pages/about 
+  # to the about action in the Static Pages controller. This automatically 
+  # creates a helper called: static_pages_about_url
+  get 'static_pages/about' 
+  
 
   root 'application#hello'
 end
