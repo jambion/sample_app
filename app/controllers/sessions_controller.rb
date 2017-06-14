d2509243fb7a7fbf6cd997c27f7810ed7ac4abaa
jambion <Ambion_J@yahoo.com>
2017-06-14 15:14:46 -0700

    Finish user edit, update, index, and destroy actions

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,7 +7,8 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
       log_in user
-      redirect_to user 
+      params[:session][:remember_me] == '1' ? remember(user) : forget(user)
+      redirect_back_or user
     else
       # Create an error message
       flash[:danger] = "Invalid email/password combination" # Not quite right!
