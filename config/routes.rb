6e1843c3f621459369d960b28764cfd4bc4f109e
jambion <Ambion_J@yahoo.com>
2017-06-14 13:04:18 -0700

    Implement basic login

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+
   get 'users/new'
 
   root 'static_pages#home'
@@ -7,11 +9,14 @@ Rails.application.routes.draw do
   
   # routes a GET request for the URL /help to the help action in the 
   # Static Pages controller
-  get 'help',     to: 'static_pages#help'
-  get 'about',    to: 'static_pages#about'
-  get 'contact',  to: 'static_pages#contact'
-  get 'signup',   to: 'users#new'
-  post '/signup',  to: 'users#create'
+  get 'help',         to: 'static_pages#help'
+  get 'about',        to: 'static_pages#about'
+  get 'contact',      to: 'static_pages#contact'
+  get 'signup',       to: 'users#new'
+  post '/signup',     to: 'users#create'
+  get '/login',       to: 'sessions#new'
+  post 'login',       to: 'sessions#create'
+  delete '/logout',   to: 'sessions#destroy'
   resources :users # RESTful Users resource and routes for generating user URLs
   
 
